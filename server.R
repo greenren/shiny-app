945251bbb769aafa0e7c25b907382864ae21b861
greenren <timmykok@gmail.com>
2014-12-21 20:46:47 +0100

    Add  plot

--- server.R
+++ server.R
@@ -7,8 +7,22 @@ shinyServer(
             mu <- input$mu
             lines(c(mu, mu), c(0, 50), col='orange', lwd=5)
             mse <- mean((faithful$waiting - mu)^2)
-            text(63, 50, paste("Guessed mean = ", mu))
-            text(63, 45, paste("Mean Squared Error = ", round(mse, 3)))
+            text(46.5, 50, paste("Guessed mean = ", mu))
+            text(50, 45, paste("Mean Squared Error = ", round(mse, 3)))
+        })
+        output$scatter <- renderPlot({
+            with(faithful, plot(eruptions, waiting, col='lightgreen', pch=8, xlab='eruption time (minutes)', ylab='waiting time (minutes)', main='Waiting time by Eruption time'))
+            with(faithful, abline(lm(waiting ~ eruptions), col='blue'))
+            fit <- lm(waiting ~ eruptions, data=faithful)
+            fitcoef <- fit$coef
+            erupttime <- input$erupttime
+            waittime <- fitcoef[1] + fitcoef[2] * erupttime
+            points(erupttime, waittime, col='red', pch=8)
+            text(1.95, 90, paste("Eruption time = ", round(erupttime, 2)))
+            text(1.9, 85, paste("Estimated waiting\ntime = ", round(waittime, 2)))
         })
     }
 )
+
+
+#predict(fit, data.frame(eruptions=c(3)), interval='confidence')
--- ui.R
+++ ui.R
@@ -2,17 +2,25 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("Waiting for Old Faithful"),
     sidebarPanel(
+        h3('Introduction'),
         img(src="https://farm5.staticflickr.com/4016/4247558649_13a7d992dc.jpg", width=500, height=333),
-        p("In this app we're looking at the faithful dataset in R. 
+        p("In this app we're looking at the 'faithful' dataset in R. 
           Which records the waiting time between eruptions and the duration of the eruption 
           for the Old Faithful geyser in Yellowstone National Park, Wyoming, USA"),
         p("We'll try to figure out how long we can expect to wait on the next eruption, given how long the previous eruption took."),
-        p("But first, you can guess the average waiting time on the right, by minimizing the Mean Sqaured Error.")
+        p("But first you can guess the average waiting time on the right, by minimizing the Mean Sqaured Error."),
+        
+        h3('Waiting Time'),
+        p("")
     ),
     
     mainPanel(
         h3('Guess the average waiting time for Old Faithful'),
-        sliderInput('mu', 'Guess the average waiting time', value=45, min=43, max=96, step=0.1),
-        plotOutput('newHist')
+        sliderInput('mu', 'Guess the average waiting time', value=53, min=43, max=96, step=0.1),
+        plotOutput('newHist'),
+        
+        h3('How long should you wait for the next eruption?'),
+        sliderInput('erupttime', 'The previous eruption took how many minutes?', value=3.5, min=1.5, max=5.2, step=0.01),
+        plotOutput('scatter')
     )
 ))
