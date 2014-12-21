491b3322378e10d7efb0d2b188160fe732a12cba
greenren <timmykok@gmail.com>
2014-12-21 19:25:13 +0100

    Add guess the mean waiting time

--- server.R
+++ server.R
@@ -1,5 +1,14 @@
 library(shiny)
+data(faitful)
 shinyServer(
     function(input, output) {  
+        output$newHist <- renderPlot ({
+            hist(faithful$waiting, xlab='Waiting Time in minutes', col='lightgreen', main='Histogram of Old Faithful\'s Waiting Times')
+            mu <- input$mu
+            lines(c(mu, mu), c(0, 50), col='orange', lwd=5)
+            mse <- mean((faithful$waiting - mu)^2)
+            text(63, 50, paste("Guessed mean = ", mu))
+            text(63, 45, paste("Mean Squared Error = ", round(mse, 3)))
+        })
     }
 )
--- ui.R
+++ ui.R
@@ -1,12 +1,18 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-    headerPanel("Some Title"),
+    headerPanel("Waiting for Old Faithful"),
     sidebarPanel(
-        img(src="https://farm5.staticflickr.com/4016/4247558649_13a7d992dc.jpg"),
-        h3('some more text')
+        img(src="https://farm5.staticflickr.com/4016/4247558649_13a7d992dc.jpg", width=500, height=333),
+        p("In this app we're looking at the faithful dataset in R. 
+          Which records the waiting time between eruptions and the duration of the eruption 
+          for the Old Faithful geyser in Yellowstone National Park, Wyoming, USA"),
+        p("We'll try to figure out how long we can expect to wait on the next eruption, given how long the previous eruption took."),
+        p("But first, you can guess the average waiting time on the right, by minimizing the Mean Sqaured Error.")
     ),
+    
     mainPanel(
-        h3('yet some more text')
-        
+        h3('Guess the average waiting time for Old Faithful'),
+        sliderInput('mu', 'Guess the average waiting time', value=45, min=43, max=96, step=0.1),
+        plotOutput('newHist')
     )
 ))
